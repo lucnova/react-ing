090f2c688e77513c4c270ea1d48b3ec6811eab10
lucnova <nlnovae@gmail.com>
2020-12-30 19:19:25 -0600

    Object Destructuring

---
+++ repasojs/js/01.js
@@ -0,0 +1,19 @@
+/* -------------------------------------------------------------------------- */
+/*                            DESTRUCTURAR OBJETOS                            */
+/* -------------------------------------------------------------------------- */
+
+/*
+* Como se extraen propiedades de objetos
+*/
+'use strict';
+
+const JS = {
+    'version': 'ES6',
+    'frameworks': ['React', 'Svelte.js', 'EmberJS']
+};
+
+
+let {version, frameworks} = JS;
+
+
+console.log(version, frameworks, c);
