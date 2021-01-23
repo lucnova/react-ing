186ec67f4a1b8495bc3f9ae857f7fa641159513b
lucnova <nlnovae@gmail.com>
2021-01-22 21:15:48 -0600

    Prettify

--- REACT/02-FirstSteps/intro-javascript/src/jstest/10-async-await.js
+++ REACT/02-FirstSteps/intro-javascript/src/jstest/10-async-await.js
@@ -4,14 +4,13 @@
 /* PROMISES SIMPLIFICADOS */
 
 /* DATOS DE LA API */
-const apiKey = 'VXAD1LH05QB9';
+const apiKey = "VXAD1LH05QB9";
 
-const searchTerm = 'pixelart';
+const searchTerm = "pixelart";
 
 const url = `https://api.tenor.com/v1/random?key=${apiKey}&q=${searchTerm}&limit=1`;
 
-
-console.info('URL Llamada:', url);
+console.info("URL Llamada:", url);
 
 /* PETICIÓN ASÍNCRONA */
 /**
@@ -23,42 +22,36 @@ const fetchAndShowGIF = async () => {
 		const myTenorFetchResponse = await fetch(url);
 		const jsonResponse = await myTenorFetchResponse.json();
 
-		const {
-			results,
-			next
-		} = jsonResponse;
+		const { results, next } = jsonResponse;
 
 		console.log(results, next);
 
 		for (const currentSearchResult of results) {
-			const gifURL = currentSearchResult.media[0].loopedmp4.url
-			console.log('GIF URL', gifURL);
+			const gifURL = currentSearchResult.media[0].loopedmp4.url;
+			console.log("GIF URL", gifURL);
 
-			const newVideo = document.createElement('video');
-			newVideo.setAttribute('controls', '');
-			newVideo.setAttribute('width', '80%');
+			const newVideo = document.createElement("video");
+			newVideo.setAttribute("controls", "");
+			newVideo.setAttribute("width", "80%");
 
-			newVideo.innerHTML =
-				`
+			newVideo.innerHTML = `
             <source src="${gifURL}" type="video/mp4">
             Your browser does not support the video tag.
             `;
 
-			document.querySelector('body').appendChild(newVideo);
+			document.querySelector("body").appendChild(newVideo);
 		}
-	}
-	catch (error) {
-        /* Manejar el error */
+	} catch (error) {
+		/* Manejar el error */
 		console.warn(error);
 	}
-}
+};
 
 fetchAndShowGIF().then(() => {
-	console.log('*Petición Terminada*');
+	console.log("*Petición Terminada*");
 });
 
 // .catch(console.warn)     // -> Esto ya no iria aca, las peticiones de async/await son con try/catch dentro de funcion
-
 
 /*
 // A DIFERENCIA DE FETCH QUE SERÍA:
