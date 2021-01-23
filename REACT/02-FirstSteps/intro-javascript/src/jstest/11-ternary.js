eadbad9722ac6730fee77e4a559318393beeb4a3
lucnova <nlnovae@gmail.com>
2021-01-22 21:35:40 -0600

    Ternary Operator

---
+++ REACT/02-FirstSteps/intro-javascript/src/jstest/11-ternary.js
@@ -0,0 +1,11 @@
+/* -------------------------------------------------------------------------- */
+/*                              OPERADOR TERNARIO                             */
+/* -------------------------------------------------------------------------- */
+
+const isActive = true;
+
+/* Basicamente, si una condicion es true -> retorna algo */
+/* if() => true ??   Entonces Devuelve... */
+const msg = isActive && 'Está Activo';
+
+console.log(msg);
