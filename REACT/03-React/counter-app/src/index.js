645d5020bf0c2f1c61264f3237f8e94290a5e7cd
lucnova <nlnovae@gmail.com>
2021-01-29 18:14:47 -0600

    Added Reference to Comment

--- REACT/03-React/counter-app/src/index.js
+++ REACT/03-React/counter-app/src/index.js
@@ -1,11 +1,13 @@
-/* NOTA: Se puede comentar esa linea y usar JSX sin problemas. */
-//import React from 'react';
+/**
+ * NOTA: Se puede comentar esa linea y usar JSX sin problemas.
+ * https://es.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+ */
+import React from 'react';
 import ReactDOM from 'react-dom';
+import FirstApp from './FirstApp';
 
-/* Declaro el objeto JSX */
-const myTitle = <h1>Hello Everyone</h1>;
 /* Obtengo la referencia del elemento DOM a renderizar el objeto */
 const myRoot = document.querySelector('#root');
 
 /* Renderizarlo */
-ReactDOM.render(myTitle, myRoot);
+ReactDOM.render(<FirstApp />, myRoot);
