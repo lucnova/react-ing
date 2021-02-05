b41a2d8ca67329b4cba5237b5904a2949bd5813b
lucnova <nlnovae@gmail.com>
2021-02-05 13:49:10 -0600

    Added Style + Comments

--- REACT/03-React/counter-app/src/index.js
+++ REACT/03-React/counter-app/src/index.js
@@ -2,12 +2,14 @@
  * NOTA: Se puede comentar esa linea y usar JSX sin problemas.
  * https://es.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FirstApp from './FirstApp';
+import React from 'react';  // COMPONENTE PRINCIPAL -> Da funciones de JSX tambien (leer nota)
+import ReactDOM from 'react-dom';   // MANIPULAR DOM
+import FirstApp from './FirstApp';  // COMPONENTE PROPIO
+
+import './index.css';
 
 /* Obtengo la referencia del elemento DOM a renderizar el objeto */
 const myRoot = document.querySelector('#root');
 
-/* Renderizarlo */
+/* Renderizarlo: render(COMPONENTE, idDOM) */
 ReactDOM.render(<FirstApp />, myRoot);
