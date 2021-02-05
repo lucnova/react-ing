73314364522d1ad073ea0f4f5c55e0111b987575
lucnova <nlnovae@gmail.com>
2021-02-05 13:49:24 -0600

    Custom Component

---
+++ REACT/03-React/counter-app/src/FirstApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+/* Functional Component */
+/**
+ * También existen otros tipos de componentes como el de Clases, pero Functional 
+ * parece ser lo que React está implementando.
+ */
+const FirstApp = () => {
+    return <h1>Yooo</h1>;
+}
+
+export default FirstApp;
