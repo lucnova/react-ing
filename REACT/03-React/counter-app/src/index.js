109435254085fbe935105bbcbaa50d7a47415480
lucnova <nlnovae@gmail.com>
2021-02-05 15:48:46 -0600

    Props Send

--- REACT/03-React/counter-app/src/index.js
+++ REACT/03-React/counter-app/src/index.js
@@ -2,9 +2,9 @@
  * NOTA: Se puede comentar esa linea y usar JSX sin problemas.
  * https://es.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
  */
-import React from 'react';  // COMPONENTE PRINCIPAL -> Da funciones de JSX tambien (leer nota)
-import ReactDOM from 'react-dom';   // MANIPULAR DOM
-import FirstApp from './FirstApp';  // COMPONENTE PROPIO
+import React from 'react'; // COMPONENTE PRINCIPAL -> Da funciones de JSX tambien (leer nota)
+import ReactDOM from 'react-dom'; // MANIPULAR DOM
+import FirstApp from './FirstApp'; // COMPONENTE PROPIO
 
 import './index.css';
 
@@ -12,4 +12,4 @@ import './index.css';
 const myRoot = document.querySelector('#root');
 
 /* Renderizarlo: render(COMPONENTE, idDOM) */
-ReactDOM.render(<FirstApp />, myRoot);
+ReactDOM.render(<FirstApp title="Eu te amo" description="macaquinho chupando pirulito azul" />, myRoot);
