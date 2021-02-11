4894e28c05f4f78a16fb6a26a72f732644ed8af0
lucnova <nlnovae@gmail.com>
2021-02-11 16:50:38 -0600

    CounterApp useState Hook - useState Hook - Substract & Reset Events Handling

--- REACT/03-React/counter-app/src/CounterApp.js
+++ REACT/03-React/counter-app/src/CounterApp.js
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // useState -> ES UN HOOK
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
-    const state = useState();
-    
+	const [counter, setCounter] = useState(0); // useState trae un arreglo con DOS VALORES [var, hacerAlgo()]
+
 	/* ------------------------------- Handle ADD ------------------------------- */
 	const handleAdd = (e) => {
-		console.log(e);
+		setCounter(counter + 1); // Settear un valor dado
+		//setCounter((el) => el + 1 );    // Funcion callback que el retorno es seteado
 	};
 
+	/* ------------------------------- Handle SUB ------------------------------- */
+	const handleSubstract = (e) => {
+		setCounter(counter - 1);
+	};
+
+	/* ------------------------------ Handle Reset ------------------------------ */
+	const handleReset = (e) => {
+		setCounter(0);
+	};
+    
 	return (
 		<>
 			<h1>CounterApp</h1>
-			<h2>{value}</h2>
+			<h2>{counter}</h2>
 
 			<button onClick={handleAdd}>+1</button>
+			<button onClick={handleSubstract}>-1</button>
+			<button onClick={handleReset}>R</button>
 		</>
 	);
 };
