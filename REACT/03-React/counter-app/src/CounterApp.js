79081e6b61f4d224502509be6c5d4ba5263814c7
lucnova <nlnovae@gmail.com>
2021-02-11 16:35:54 -0600

    Event Handling + useState inclution

--- REACT/03-React/counter-app/src/CounterApp.js
+++ REACT/03-React/counter-app/src/CounterApp.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = (value) => {
+const CounterApp = ({ value }) => {
+    const state = useState();
+    
+	/* ------------------------------- Handle ADD ------------------------------- */
+	const handleAdd = (e) => {
+		console.log(e);
+	};
+
 	return (
 		<>
 			<h1>CounterApp</h1>
 			<h2>{value}</h2>
+
+			<button onClick={handleAdd}>+1</button>
 		</>
 	);
 };
 
 CounterApp.propTypes = {
-    value: PropTypes.number,
+	value: PropTypes.number,
 };
 
 export default CounterApp;
