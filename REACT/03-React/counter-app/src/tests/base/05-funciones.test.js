6214133a456d610e066ecc28364ea81faaf5d5d9
lucnova <nlnovae@gmail.com>
2021-02-12 19:09:05 -0600

    Testing on 05-funciones

---
+++ REACT/03-React/counter-app/src/tests/base/05-funciones.test.js
@@ -0,0 +1,26 @@
+import { getUser, getActiveUser } from '../../base/05-funciones';
+
+describe('-testing 05-funciones', () => {
+	test('"getUser" MUST return an Object', () => {
+		const userExpected = {
+			uid: 'ABC123',
+			username: 'El_Papi1502',
+		};
+
+		const userResult = getUser();
+
+		expect(userResult).toEqual(userExpected);
+	});
+
+	test('"getActiveUser" MUST return an Object', () => {
+		const testUsername = 'lucnova';
+		const userExpected = {
+			uid: 'ABC567',
+			username: testUsername,
+		};
+
+		const userResult = getActiveUser(testUsername);
+
+		expect(userResult).toEqual(userExpected);
+	});
+});
