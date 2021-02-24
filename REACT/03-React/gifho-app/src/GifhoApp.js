7c6be74af5da1e5db6aeae762572963eeec71483
lucnova <nlnovae@gmail.com>
2021-02-24 16:54:06 -0600

    AddCategory Component Added

--- REACT/03-React/gifho-app/src/GifhoApp.js
+++ REACT/03-React/gifho-app/src/GifhoApp.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+//import PropTypes from 'prop-types';
+
+import AddCategory from './components/AddCategory';
 
 const GifhoApp = (props) => {
+	const [categories, setCategories] = useState(['Pixel Art', 'Vinesauce', 'DDLC']);
+
 	return (
 		<>
-			<h2>Gifho!</h2>
-			<hr />
+			<div>
+				<h2>Gifho!</h2>
+				<hr />
+			</div>
+
+			<AddCategory setCategories={setCategories} />
+
+			<div>
+				<ol>
+					{categories.map((category) => {
+						return <li key={category}>{category}</li>;
+					})}
+				</ol>
+			</div>
 		</>
 	);
 };
 
-GifhoApp.propTypes = {};
+//GifhoApp.propTypes = {};
 
 export default GifhoApp;
---
+++ REACT/03-React/gifho-app/src/components/AddCategory.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddCategory = ({ setCategories }) => {
+	const [inputValue, setInputValue] = useState(''); // Si se deja vacio, el valor es undefined y handleChange Colapsa
+
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const trimmedValue = inputValue.trim();
+
+		if (trimmedValue.length > 2) {
+			setCategories((currCategories) => {
+				if (currCategories.findIndex((item) => item === trimmedValue) === -1) {
+					setInputValue(''); // Limpiar Input
+					return [...currCategories, trimmedValue];
+				} else {
+					return currCategories;
+				}
+			});
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type="text" value={inputValue} placeholder="Category Name" onChange={handleInputChange}></input>
+			<button>Add Category</button>
+		</form>
+	);
+};
+
+AddCategory.propTypes = {
+	setCategories: PropTypes.func.isRequired,
+};
+
+export default AddCategory;
