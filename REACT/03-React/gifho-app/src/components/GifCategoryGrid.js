e419c6be5d41c9bcb147dfe36c310ced1f8a7e3b
lucnova <nlnovae@gmail.com>
2021-02-24 17:55:50 -0600

    GifCategoryGridItem + UseEffect useEffect -> Used to execute code under condition

--- REACT/03-React/gifho-app/src/components/GifCategoryGrid.js
+++ REACT/03-React/gifho-app/src/components/GifCategoryGrid.js
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; // useEffect -> Ejecutar código de manera condicional
 import PropTypes from 'prop-types';
+import { GifCategoryGridItem } from './GifCategoryGridItem';
 
 const GifCategoryGrid = ({ categoryName }) => {
-	const getGifs = async (searchTerm = 'JoJo') => {
-        let sanitizedSearchTerm = searchTerm.trim().replace(' ', '+');
-        
+	const [imgs, setImgs] = useState([]);
+
+	//  - USE EFECT -
+	//      Evita renderizar todo el código de nuevo para cuando hay cambios pequeños
+	//      -> (callback, lista de dependencias)
+	//  Si se manda vacia la lista de dependencias entonces solo se ejecuta 1 vez
+	useEffect(() => {
+		getGifs();
+	}, []);
+
+	const getGifs = async (searchTerm = 'DDLC') => {
+		let sanitizedSearchTerm = searchTerm.trim().replace(' ', '+');
+
 		const apiKey = 'VXAD1LH05QB9';
-		const url = `https://g.tenor.com/v1/search?q="${sanitizedSearchTerm}"&limit=3&key="${apiKey}"`;
+		const url = `https://g.tenor.com/v1/random?q="${sanitizedSearchTerm}"&contentfilter=off&key=${apiKey}&limit=5`;
 
 		try {
 			const myTenorFetchResponse = await fetch(url);
 			const jsonResponse = await myTenorFetchResponse.json();
 
 			const { results } = jsonResponse;
-            
-            console.log(results);
 
-            /*
-			for (const currentSearchResult of results) {
-				const gifURL = currentSearchResult.media[0].loopedmp4.url;
-				return gifURL;
-			}
-            */
+			const gifsData = results.map((gifInfo) => {
+				return {
+					id: gifInfo.id,
+					title: gifInfo.url,
+					url: gifInfo.media[0].gif.url,
+				};
+			});
 
-            const gifsData = results.map((gifInfo) => {
-                return {
-                    id: gifInfo.id,
-                    title: gifInfo.h1_title,
-                    url: gifInfo.url,
-                }
-            });
+			setImgs(gifsData);
 		} catch (error) {
 			return error;
 		}
 	};
 
-    getGifs();
-
 	return (
-		<>
+		<div>
 			<h3>{categoryName}</h3>
-		</>
+			<div>
+				{imgs.map((img) => (
+					<GifCategoryGridItem key={img.id} {...img} />
+				))}
+			</div>
+		</div>
 	);
 };
 
