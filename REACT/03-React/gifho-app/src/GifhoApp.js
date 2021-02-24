7f8976de528d181ac14f3d053ea09f26526f2b52
lucnova <nlnovae@gmail.com>
2021-02-24 14:30:42 -0600

    GiphoApp Added

---
+++ REACT/03-React/gifho-app/src/GifhoApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GifhoApp = (props) => {
+	return (
+		<>
+			<h2>Gifho!</h2>
+			<hr />
+		</>
+	);
+};
+
+GifhoApp.propTypes = {};
+
+export default GifhoApp;
