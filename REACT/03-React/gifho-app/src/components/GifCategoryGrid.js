9e705e79525a895e8c11b8240e8a9edf765818f7
lucnova <nlnovae@gmail.com>
2021-02-25 17:36:57 -0600

    Code Moved as getGifs helper + className added

--- REACT/03-React/gifho-app/src/components/GifCategoryGrid.js
+++ REACT/03-React/gifho-app/src/components/GifCategoryGrid.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'; // useEffect -> Ejecutar código de manera condicional
 import PropTypes from 'prop-types';
 import { GifCategoryGridItem } from './GifCategoryGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 const GifCategoryGrid = ({ categoryName }) => {
 	const [imgs, setImgs] = useState([]);
@@ -10,44 +11,22 @@ const GifCategoryGrid = ({ categoryName }) => {
 	//      -> (callback, lista de dependencias)
 	//  Si se manda vacia la lista de dependencias entonces solo se ejecuta 1 vez
 	useEffect(() => {
-		getGifs();
-	}, []);
-
-	const getGifs = async (searchTerm = 'DDLC') => {
-		let sanitizedSearchTerm = searchTerm.trim().replace(' ', '+');
-
-		const apiKey = 'VXAD1LH05QB9';
-		const url = `https://g.tenor.com/v1/random?q="${sanitizedSearchTerm}"&contentfilter=off&key=${apiKey}&limit=5`;
-
-		try {
-			const myTenorFetchResponse = await fetch(url);
-			const jsonResponse = await myTenorFetchResponse.json();
-
-			const { results } = jsonResponse;
-
-			const gifsData = results.map((gifInfo) => {
-				return {
-					id: gifInfo.id,
-					title: gifInfo.url,
-					url: gifInfo.media[0].gif.url,
-				};
+		getGifs(categoryName)
+			.then(setImgs)
+			.catch((error) => {
+				console.error(error);
 			});
-
-			setImgs(gifsData);
-		} catch (error) {
-			return error;
-		}
-	};
+	}, [categoryName]); // El arreglo contiene variables de las cuales useEffect se ejecutará denuevo
 
 	return (
-		<div>
+		<>
 			<h3>{categoryName}</h3>
-			<div>
+			<div className="card-grid">
 				{imgs.map((img) => (
 					<GifCategoryGridItem key={img.id} {...img} />
 				))}
 			</div>
-		</div>
+		</>
 	);
 };
 
