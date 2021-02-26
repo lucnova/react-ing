9fdd9866dea232f9878d700f12dc3426b019f0f5
lucnova <nlnovae@gmail.com>
2021-02-25 23:10:03 -0600

    Used p instead of a for styling

--- REACT/03-React/gifho-app/src/components/GifCategoryGridItem.js
+++ REACT/03-React/gifho-app/src/components/GifCategoryGridItem.js
@@ -4,7 +4,7 @@ export const GifCategoryGridItem = (props) => {
 	return (
 		<div className="card animate__animated animate__bounceIn">
 			<img src={props.url} alt={props.title}></img>
-			<a href={props.title} target="_blank" rel="noreferrer">{props.title}</a>
+			<p>{props.title}</p>
 		</div>
 	);
 };
