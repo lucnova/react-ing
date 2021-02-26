1bbdeebd1cae24b05e87f61781c31418bec02575
lucnova <nlnovae@gmail.com>
2021-02-25 18:03:40 -0600

    Commented Code+Added a Custom Hook2Handle Loading

--- REACT/03-React/gifho-app/src/components/GifCategoryGrid.js
+++ REACT/03-React/gifho-app/src/components/GifCategoryGrid.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react'; // useEffect -> Ejecutar código de manera condicional
+import React /*  useState, useEffect */ from 'react'; // useEffect -> Ejecutar código de manera condicional
 import PropTypes from 'prop-types';
-import { GifCategoryGridItem } from './GifCategoryGridItem';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+// import { GifCategoryGridItem } from './GifCategoryGridItem';
+// import { getGifs } from '../helpers/getGifs';
 
 const GifCategoryGrid = ({ categoryName }) => {
-	const [imgs, setImgs] = useState([]);
+	// const [imgs, setImgs] = useState([]);
+	const {loading, data} = useFetchGifs();
+
+	console.log(loading);
 
 	//  - USE EFECT -
 	//      Evita renderizar todo el código de nuevo para cuando hay cambios pequeños
 	//      -> (callback, lista de dependencias)
 	//  Si se manda vacia la lista de dependencias entonces solo se ejecuta 1 vez
-	useEffect(() => {
-		getGifs(categoryName)
-			.then(setImgs)
-			.catch((error) => {
-				console.error(error);
-			});
-	}, [categoryName]); // El arreglo contiene variables de las cuales useEffect se ejecutará denuevo
+	// useEffect(() => {
+	// 	getGifs(categoryName)
+	// 		.then(setImgs)
+	// 		.catch((error) => {
+	// 			console.error(error);
+	// 		});
+	// }, [categoryName]); // El arreglo contiene variables de las cuales useEffect se ejecutará denuevo
 
 	return (
 		<>
 			<h3>{categoryName}</h3>
-			<div className="card-grid">
+			{/* <div className="card-grid">
 				{imgs.map((img) => (
 					<GifCategoryGridItem key={img.id} {...img} />
 				))}
-			</div>
+			</div> */}
+			{loading ? 'Loading...' : 'DONE'}
 		</>
 	);
 };
