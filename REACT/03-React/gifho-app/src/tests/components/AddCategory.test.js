2376ba6d9f3e4d364492fc45c6221deac02fe629
lucnova <nlnovae@gmail.com>
2021-03-03 16:42:02 -0600

    Testing on Input

--- REACT/03-React/gifho-app/src/tests/components/AddCategory.test.js
+++ REACT/03-React/gifho-app/src/tests/components/AddCategory.test.js
@@ -2,9 +2,26 @@ import { shallow } from 'enzyme';
 import AddCategory from '../../components/AddCategory';
 
 describe('Testing on AddCategory', () => {
-	test('should display component as snapshot', () => {
-		const wrapper = shallow(<AddCategory setCategories={() => {}} />);
+	const wrapper = shallow(<AddCategory setCategories={() => {}} />);
 
+	test('should display component as snapshot', () => {
 		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('should display changed value inside input text', () => {
+		const sentValue = 'Hey';
+		const input = wrapper.find('input[type="text"]');
+
+		input.simulate('change', { target: { value: sentValue } });
+
+		expect(wrapper.find('#inputValueCheck').text().trim()).toBe(sentValue);
+
+        /*
+        ESTO DA ERROR:
+        
+		const inputValue = wrapper.find('#inputValueCheck');
+        ...
+        expect(inputValue.text().trim()).toBe(sentValue);
+        */
 	});
 });
