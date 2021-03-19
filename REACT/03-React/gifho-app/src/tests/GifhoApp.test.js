d11d2ebfba8456c94a88b770dd578c7e2ec81d03
lucnova <nlnovae@gmail.com>
2021-03-19 14:05:35 -0600

    Moved File

--- REACT/03-React/gifho-app/src/tests/GifhoApp.test.js
+++ REACT/03-React/gifho-app/src/tests/GifhoApp.test.js
@@ -1,5 +1,5 @@
 import { shallow } from 'enzyme';
-import GifhoApp from '../../GifhoApp';
+import GifhoApp from '../GifhoApp';
 
 describe('testing on GifhoApp', () => {
 	test('should match snapshot', () => {
