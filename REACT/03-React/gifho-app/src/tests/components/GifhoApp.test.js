92da36e494b621dd6474987762b91d6fa14bd42b
lucnova <nlnovae@gmail.com>
2021-03-19 14:03:37 -0600

    Initial Test on GifhoApp

---
+++ REACT/03-React/gifho-app/src/tests/components/GifhoApp.test.js
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme';
+import GifhoApp from '../../GifhoApp';
+
+describe('testing on GifhoApp', () => {
+	test('should match snapshot', () => {
+		const wrapper = shallow(<GifhoApp />);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+});
