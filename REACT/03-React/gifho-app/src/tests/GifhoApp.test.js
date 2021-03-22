c29d08cb0658f1dde0e40ef6b9cbaf7e1d75e5a6
lucnova <nlnovae@gmail.com>
2021-03-22 00:10:00 -0600

    Testing on GifhoApp

--- REACT/03-React/gifho-app/src/tests/GifhoApp.test.js
+++ REACT/03-React/gifho-app/src/tests/GifhoApp.test.js
@@ -1,10 +1,27 @@
 import { shallow } from 'enzyme';
 import GifhoApp from '../GifhoApp';
 
+/* No es posible evaluar un hook useState en Jest */
+
 describe('testing on GifhoApp', () => {
 	test('should match snapshot', () => {
 		const wrapper = shallow(<GifhoApp />);
 
 		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('should display NO GifGrid components', () => {
+		const wrapper = shallow(<GifhoApp defaultCategories={[]} />);
+
+		expect(wrapper).toMatchSnapshot();
+		expect(wrapper.find('GifCategoryGrid').length).toBe(0);
+	});
+
+	test('should display SAME LENGTH of GifGrid components', () => {
+		const categoriesSent = ['Pixel Art', 'Vinesauce', 'DDLC'];
+		const wrapper = shallow(<GifhoApp defaultCategories={categoriesSent} />);
+
+		expect(wrapper).toMatchSnapshot();
+		expect(wrapper.find('GifCategoryGrid').length).toBe(categoriesSent.length);
 	});
 });
