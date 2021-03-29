deb9b86f058620cc13f19141d602c2ee0cd59122
lucnova <nlnovae@gmail.com>
2021-03-28 21:23:22 -0600

    useEffect cleanup used

--- REACT/03-React/hook-app/src/components/02-useEffect/Message.js
+++ REACT/03-React/hook-app/src/components/02-useEffect/Message.js
@@ -1,18 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
+	const [coords, setCoords] = useState({
+		x: 0,
+		y: 0,
+	});
+
+	const { x, y } = coords;
+
 	useEffect(() => {
 		console.log('* Message Component Mounted *');
 
-        // useEffect -> Este return es para una funcion que se ejecutará cuando el componente se desmonta. Útil para limpieza
+		const showCoords = (e) => {
+			const coords = { x: e.x, y: e.y };
+
+			setCoords(coords);
+		};
+
+		// EJEMPLO DE LIMPIEZA: Si no se limpia el siguiente evento entonces la memoria será consumida; el addEventListener es agregado N cantidad de veces
+		window.addEventListener('mousemove', showCoords);
+
+		// useEffect -> Este return es para una funcion que se ejecutará cuando el componente se desmonta. Útil para limpieza
 		return () => {
 			console.log('* Message Component Un-Mounted *');
+			window.removeEventListener('mousemove', showCoords);
 		};
 	}, []);
 
 	return (
 		<>
-			<h3>Hello There.</h3>
+			<h3>I know where you are 👀</h3>
+			<p>
+				x: {x}, y: {y}
+			</p>
 		</>
 	);
 };
