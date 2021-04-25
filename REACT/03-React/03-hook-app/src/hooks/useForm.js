46d3bb303a19657a1a2f2191a5ad7c814843bd1a
lucnova <nlnovae@gmail.com>
2021-04-25 14:05:59 -0500

    Added a Reset Form Values Function

--- REACT/03-React/03-hook-app/src/hooks/useForm.js
+++ REACT/03-React/03-hook-app/src/hooks/useForm.js
@@ -2,7 +2,7 @@
  *  USE FORM HOOK
  *      Con este hook puedo manejar campos de un form.
  *      El estado inicial es un objeto donde cada propiedad es un valor de un input
- * 
+ *
  *      Regresa el estado actual con una funcion para manejar el cambio de un input.
  */
 
@@ -12,11 +12,15 @@ export const useForm = (initialState = {}) => {
 	const [formValues, setFormValues] = useState(initialState);
 
 	const handleInputChange = ({ target }) => {
-        setFormValues({
-            ...formValues,      // Mantener las posibles propiedades anteriores
-			[target.name]: target.value,    // Poner la nueva propiedad en su valor
+		setFormValues({
+			...formValues, // Mantener las posibles propiedades anteriores
+			[target.name]: target.value, // Poner la nueva propiedad en su valor
 		});
 	};
 
-    return [formValues, handleInputChange];     //Regresa el estado actual con una funcion para manejar el cambio de un input.
+	const resetFormValues = () => {
+		setFormValues(initialState);
+	};
+
+	return [formValues, handleInputChange, resetFormValues]; //Regresa el estado actual con una funcion para manejar el cambio de un input.
 };
