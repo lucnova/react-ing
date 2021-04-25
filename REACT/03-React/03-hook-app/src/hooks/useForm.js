f8f9d51bd1c93e038f5e579c23e9cfc083a2c95c
lucnova <nlnovae@gmail.com>
2021-04-25 13:45:42 -0500

    useForm Documented

--- REACT/03-React/03-hook-app/src/hooks/useForm.js
+++ REACT/03-React/03-hook-app/src/hooks/useForm.js
@@ -1,14 +1,22 @@
+/**
+ *  USE FORM HOOK
+ *      Con este hook puedo manejar campos de un form.
+ *      El estado inicial es un objeto donde cada propiedad es un valor de un input
+ * 
+ *      Regresa el estado actual con una funcion para manejar el cambio de un input.
+ */
+
 import { useState } from 'react';
 
 export const useForm = (initialState = {}) => {
 	const [formValues, setFormValues] = useState(initialState);
 
 	const handleInputChange = ({ target }) => {
-		setFormValues({
-			...formValues,
-			[target.name]: target.value,
+        setFormValues({
+            ...formValues,      // Mantener las posibles propiedades anteriores
+			[target.name]: target.value,    // Poner la nueva propiedad en su valor
 		});
 	};
 
-    return [formValues, handleInputChange];
+    return [formValues, handleInputChange];     //Regresa el estado actual con una funcion para manejar el cambio de un input.
 };
