8f9ab7618bad3e01d98d7b2d30cb4e6985e2b9d4
lucnova <nlnovae@gmail.com>
2021-04-25 16:16:17 -0500

    Added Delete Case on Reducer

--- REACT/03-React/03-hook-app/src/components/08-useReducer/todoReducer.js
+++ REACT/03-React/03-hook-app/src/components/08-useReducer/todoReducer.js
@@ -2,6 +2,8 @@ const todoReducer = (state = [], action) => {
 	switch (action?.type) {
 		case 'add':
 			return [...state, action.payload];
+		case 'delete':
+			return state.filter((todo) => todo.id !== action.payload); // El payload será el ID
 		default:
 			return state;
 	}
