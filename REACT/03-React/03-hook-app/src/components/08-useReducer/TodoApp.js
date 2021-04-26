f306f8abab3353a2d869aeda3163714c6326c981
lucnova <nlnovae@gmail.com>
2021-04-25 19:56:07 -0500

    handleToggleTodo + Conditional Rendering

--- REACT/03-React/03-hook-app/src/components/08-useReducer/TodoApp.js
+++ REACT/03-React/03-hook-app/src/components/08-useReducer/TodoApp.js
@@ -79,6 +79,15 @@ export const TodoApp = () => {
 		dispatch(deleteTodoAction);
 	};
 
+	const handleToggleTodo = (todoID) => {
+		const toggleTodoAction = {
+			type: 'toggle',
+			payload: todoID,
+		};
+
+		dispatch(toggleTodoAction);
+	};
+
 	// ** CONTENIDO **
 
 	return (
@@ -100,21 +109,22 @@ export const TodoApp = () => {
 					<div className="row row-cols-1 row-cols-md-2 g-4">
 						{currentTodos.map((todo) => (
 							<div key={todo.id} className="col">
-								<div className="card">
+								<div className={`${todo.done === true ? 'card text-decoration-line-through' : 'card'}`}>
 									<div className="card-body">
 										<div className="row">
 											<div className="col">
 												<h5 className="card-title">{todo.title}</h5>
 												<h6 className="card-subtitle mb-2 text-muted">{new Date(todo.id).toLocaleString()}</h6>
 												<p className="card-text">{todo.desc}</p>
-
-												<button className="btn btn-danger" onClick={() => handleDeleteTodo(todo.id)}>
+												<button className="btn btn-danger" onClick={() => handleDeleteTodo(todo.id)} disabled={todo.done}>
 													Delete
 												</button>
 											</div>
 
 											<div className="col text-center m-auto">
-												<button className="btn btn-primary p-4">DONE</button>
+												<button className="btn btn-primary p-4" onClick={() => handleToggleTodo(todo.id)}>
+													{todo.done === false ? 'Mark' : 'Unmark'}
+												</button>
 											</div>
 										</div>
 									</div>
