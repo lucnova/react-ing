09a48302d5b987324d4316d0b4b6ea2cebd3ec4f
lucnova <nlnovae@gmail.com>
2021-05-01 16:47:38 -0500

    UserContext Get on Home

--- REACT/03-React/03-hook-app/src/components/09-useContext/HomePage.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/HomePage.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomePage = () => {
+	const userContext = useContext(UserContext);
+    console.log("🚀 ~ file: HomePage.js ~ line 6 ~ HomePage ~ userContext", userContext)
+
 	return (
 		<>
 			<h1>Home Page</h1>
