2fa19179e0c602cce116bfaab89b38b69eae745f
lucnova <nlnovae@gmail.com>
2021-05-01 17:06:04 -0500

    useState Used

--- REACT/03-React/03-hook-app/src/components/09-useContext/MainApp.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/MainApp.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppRouter } from './AppRouter';
 import { UserContext } from './UserContext';
 
 // * Establecer el contexto a todo el componente principal.
 
 export const MainApp = () => {
-	const user = {
-		id: '12ABC',
-		username: 'lucnova',
-		email: 'nlnovae@gmail.com',
-	};
+	const [user, setUser] = useState({});
 
 	return (
-		<UserContext.Provider value={user}>
+		<UserContext.Provider
+			value={{
+				user,
+				setUser,
+			}}
+		>
 			<AppRouter />
 		</UserContext.Provider>
 	);
