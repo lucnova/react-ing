860b7fb566b76e3fdb71318cddb2f6bbcd996d3c
lucnova <nlnovae@gmail.com>
2021-05-01 16:23:37 -0500

    NavBar Default Case

--- REACT/03-React/03-hook-app/src/components/09-useContext/AppRouter.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/AppRouter.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import { NavBar } from './NavBar';
 import { AboutPage } from './AboutPage';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
-import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
 	// * El Switch devuelve lo primero que se indique; si pones Route path='/', no importa si te dirijes a /about;
@@ -15,10 +16,12 @@ export const AppRouter = () => {
 				<div>
 					<NavBar />
 					<Switch>
+						<Route exact path="/" component={HomePage} />
 						<Route exact path="/home" component={HomePage} />
 						<Route exact path="/login" component={LoginPage} />
 						<Route exact path="/about" component={AboutPage} />
-						<Route exact path="/" component={HomePage} />
+
+						<Redirect to="/" />
 					</Switch>
 				</div>
 			</Router>
