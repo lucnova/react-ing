54d82432c2a639e372caf9241485f11cbfeeea2a
lucnova <nlnovae@gmail.com>
2021-05-01 17:16:49 -0500

    Login Logout Buttons using context

--- REACT/03-React/03-hook-app/src/components/09-useContext/LoginPage.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/LoginPage.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const LoginPage = () => {
+	const { setUser } = useContext(UserContext);
+
 	return (
 		<>
 			<h1>Login Page</h1>
 			<hr />
+
+			<button
+				className="btn btn-primary"
+				onClick={() =>
+					setUser({
+						id: '123ABC',
+						username: 'lucnova',
+						email: 'alanugb@gmail.com',
+					})
+				}
+			>
+				Login
+			</button>
+
+			<button className="btn btn-warning" onClick={() => setUser({})}>
+				Logout
+			</button>
 		</>
 	);
 };
