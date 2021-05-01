2adad2825d42d9a3cf1dc48772beee7d24f6d599
lucnova <nlnovae@gmail.com>
2021-05-01 13:48:22 -0500

    AppRouter Router Component

---
+++ REACT/03-React/03-hook-app/src/components/09-useContext/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+
+export const AppRouter = () => {
+	// * El Switch devuelve lo primero que se indique; si pones Route path='/', no importa si te dirijes a /about;
+    //       coinciden el / y es suficiente para el Switch,
+    //      Para hacerlo exacto, colocar 'exact' como propiedad.
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route exact path="/home" component={HomePage} />
+					<Route exact path="/login" component={LoginPage} />
+					<Route exact path="/about" component={AboutPage} />
+					<Route exact path="/" component={HomePage} />
+				</Switch>
+			</div>
+		</Router>
+	);
+};
