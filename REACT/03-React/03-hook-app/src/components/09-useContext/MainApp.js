52687e4cd6f8c8f5584db1970851a45a1ffa85ef
lucnova <nlnovae@gmail.com>
2021-05-01 13:48:13 -0500

    Substituted for AppRouter

--- REACT/03-React/03-hook-app/src/components/09-useContext/MainApp.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/MainApp.js
@@ -1,26 +1,6 @@
 import React from 'react';
+import { AppRouter } from './AppRouter';
 
 export const MainApp = () => {
-	return (
-		<>
-			<nav className="navbar navbar-expand-lg navbar-dark bg-primary px-3">
-				<h2 className="navbar-brand">Navbar</h2>
-
-				<div className="collapse navbar-collapse pt-3">
-					<ul className="navbar-nav mr-auto">
-						<li className="nav-item active">
-							<p className="nav-link" href="#">
-								Home
-							</p>
-						</li>
-						<li className="nav-item">
-							<p className="nav-link" href="#">
-								Link
-							</p>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</>
-	);
+	return <AppRouter />;
 };
