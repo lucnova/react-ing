24b8e0b0ec8aa8d02675dd2495f1df04621ed0e4
lucnova <nlnovae@gmail.com>
2021-05-01 16:47:29 -0500

    UserContext On Whole Main App

--- REACT/03-React/03-hook-app/src/components/09-useContext/MainApp.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/MainApp.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
+
+// * Establecer el contexto a todo el componente principal.
 
 export const MainApp = () => {
-	return <AppRouter />;
+	const user = {
+		id: '12ABC',
+		username: 'lucnova',
+		email: 'nlnovae@gmail.com',
+	};
+
+	return (
+		<UserContext.Provider value={user}>
+			<AppRouter />
+		</UserContext.Provider>
+	);
 };
