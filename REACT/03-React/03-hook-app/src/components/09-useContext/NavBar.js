aeb739edf5f8d683b7f1f7edf7ffc680d59f264b
lucnova <nlnovae@gmail.com>
2021-05-01 16:33:04 -0500

    Link to NavLink

--- REACT/03-React/03-hook-app/src/components/09-useContext/NavBar.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/NavBar.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-primary px-3">
-			<h2 className="navbar-brand">Navbar</h2>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+			<Link to="/" className="navbar-brand">
+				UseContext
+			</Link>
 
-			<div className="collapse navbar-collapse pt-3">
+			<div className="collapse navbar-collapse">
 				<ul className="navbar-nav mr-auto">
-					<li className="nav-item active">
-						<Link className="nav-link" to="/">
-							Home
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="/about">
-							About
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="/login">
-							Login
-						</Link>
-					</li>
+					<NavLink to="/home" className="nav-item nav-link" activeClassName="active">
+						Home
+					</NavLink>
+					<NavLink to="/about" className="nav-item nav-link" activeClassName="active">
+						About
+					</NavLink>
+					<NavLink to="/login" className="nav-item nav-link" activeClassName="active">
+						Login
+					</NavLink>
 				</ul>
 			</div>
 		</nav>
