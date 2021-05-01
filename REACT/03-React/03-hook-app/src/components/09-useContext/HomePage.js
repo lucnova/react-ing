038e2658bdc50a2aca9759c01f91697eea71d051
lucnova <nlnovae@gmail.com>
2021-05-01 17:16:34 -0500

    User Shown on HTML

--- REACT/03-React/03-hook-app/src/components/09-useContext/HomePage.js
+++ REACT/03-React/03-hook-app/src/components/09-useContext/HomePage.js
@@ -3,12 +3,14 @@ import { UserContext } from './UserContext';
 
 export const HomePage = () => {
 	const userContext = useContext(UserContext);
-    console.log("🚀 ~ file: HomePage.js ~ line 6 ~ HomePage ~ userContext", userContext)
+	const { user } = userContext;
 
 	return (
 		<>
 			<h1>Home Page</h1>
 			<hr />
+
+			<pre>{JSON.stringify(user, null, 4)}</pre>
 		</>
 	);
 };
