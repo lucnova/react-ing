1e55723048c59fd9b639fa47e6815f746ed341b7
lucnova <nlnovae@gmail.com>
2021-05-02 17:43:54 -0500

    Testing on useCounter

--- REACT/03-React/03-hook-app/src/tests/hooks/useCounter.test.js
+++ REACT/03-React/03-hook-app/src/tests/hooks/useCounter.test.js
@@ -1,16 +1,66 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter';
 
 describe('Testing on useCounter', () => {
 	test('should return default values', () => {
-		const { result } = renderHook(() => {
-			return useCounter();
+		const { result } = renderHook(() => useCounter());
+
+		expect(result.current.count).toBe(0);
+		expect(typeof result.current.increment).toBe('function');
+		expect(typeof result.current.decrement).toBe('function');
+		expect(typeof result.current.reset).toBe('function');
+		expect(typeof result.current.setCount).toBe('function');
+	});
+
+	test('should return counter value equal to the one being sent', () => {
+		const startingCounterValue = 100;
+		const { result } = renderHook(() => useCounter(startingCounterValue));
+
+		expect(result.current.count).toBe(startingCounterValue);
+	});
+
+	test('should increment counter when calling increment() function', () => {
+		const startingCounterValue = 5;
+		const { result } = renderHook(() => useCounter(startingCounterValue));
+		const { increment } = result.current;
+
+		act(() => {
+			increment();
 		});
 
-        expect(result.current.count).toBe(0);
-        expect(typeof result.current.increment).toBe('function');
-        expect(typeof result.current.decrement).toBe('function');
-        expect(typeof result.current.reset).toBe('function');
-        expect(typeof result.current.setCount).toBe('function');
+		const { count } = result.current;
+
+		expect(count).toBe(startingCounterValue + 1);
+	});
+
+	test('should decrement counter when calling decrement() function', () => {
+		const startingCounterValue = 5;
+		const { result } = renderHook(() => useCounter(startingCounterValue));
+		const { decrement } = result.current;
+
+		act(() => {
+			decrement();
+		});
+
+		const { count } = result.current;
+
+		expect(count).toBe(startingCounterValue - 1);
+	});
+
+	test('should reset counter after calling increment() & decrement() function', () => {
+		const startingCounterValue = 5;
+		const { result } = renderHook(() => useCounter(startingCounterValue));
+		const { increment, decrement, reset } = result.current;
+
+		act(() => {
+			increment();
+			increment();
+			decrement();
+			reset();
+		});
+
+		const { count } = result.current;
+
+		expect(count).toBe(startingCounterValue);
 	});
 });
