5dbed1bd609569edc44e3f082afac8a649a2ee06
lucnova <nlnovae@gmail.com>
2021-05-15 19:10:04 -0500

    Todo's Fixtures

---
+++ REACT/03-React/03-hook-app/src/tests/fixtures/demoTodos.js
@@ -0,0 +1,15 @@
+// * DATOS FIJOS PARA PROBAR COSAS DE ToDo's.
+export const demoTodos = [
+	{
+		id: 1,
+		title: 'Test',
+		desc: 'TestDesc',
+		done: false,
+	},
+	{
+		id: 2,
+		title: 'Test2',
+		desc: 'TestDesc2',
+		done: false,
+	},
+];
