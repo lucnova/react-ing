ca6a7bb1cfd5d628fcbabe8e09e7310ccb0e99c3
lucnova <nlnovae@gmail.com>
2021-05-23 18:55:42 -0500

    Main Page Components

---
+++ REACT/03-React/04-heroes-app/src/DC/DCPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DCPage = () => {
+	return (
+		<div>
+			<h1>DC PAGE</h1>
+		</div>
+	);
+};
---
+++ REACT/03-React/04-heroes-app/src/MARVEL/MarvelPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MarvelPage = () => {
+	return (
+		<div>
+			<h1>MARVEL PAGE</h1>
+		</div>
+	);
+};
