b3799bc830fad00abe8d6f7d4f85e92955dc7d24
lucnova <nlnovae@gmail.com>
2021-05-25 11:59:28 -0500

    Made a Second Router - Redirects pages with a navbar included - Login page doesnt have the navbar

--- REACT/03-React/04-heroes-app/src/routers/AppRouter.js
+++ REACT/03-React/04-heroes-app/src/routers/AppRouter.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import { NavBar } from '../components/UI/NavBar';
 import { LoginPage } from '../components/login/LoginPage';
-import { DCPage } from '../components/DC/DCPage';
-import { MarvelPage } from '../components/MARVEL/MarvelPage';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
 	return (
 		<Router>
 			<div>
-				<NavBar />
-
 				<Switch>
 					<Route exact path="/login" component={LoginPage} />
-					<Route exact path="/dc" component={DCPage} />
-					<Route exact path="/marvel" component={MarvelPage} />
+
+					<Route path="/" component={DashboardRoutes} />
 				</Switch>
 			</div>
 		</Router>
---
+++ REACT/03-React/04-heroes-app/src/routers/DashboardRoutes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+
+import { NavBar } from '../components/UI/NavBar';
+import { HeroesPage } from '../components/heroes/HeroesPage';
+import { MarvelPage } from '../components/MARVEL/MarvelPage';
+import { DCPage } from '../components/DC/DCPage';
+
+export const DashboardRoutes = () => {
+	return (
+		<>
+			<NavBar />
+
+			<div>
+				<Switch>
+					<Route exact path="/dc" component={DCPage} />
+					<Route exact path="/marvel" component={MarvelPage} />
+					<Route exact path="/heroe/:heroID" component={HeroesPage} />
+
+					<Redirect to="/marvel" />
+				</Switch>
+			</div>
+		</>
+	);
+};
