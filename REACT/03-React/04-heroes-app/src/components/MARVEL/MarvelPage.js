6578a07581cb2b6b6732de11cf207f812ea04d36
lucnova <nlnovae@gmail.com>
2021-06-04 13:54:46 -0500

    HeroesList Added

--- REACT/03-React/04-heroes-app/src/components/MARVEL/MarvelPage.js
+++ REACT/03-React/04-heroes-app/src/components/MARVEL/MarvelPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { HeroesList } from '../heroes/HeroesList';
 
 export const MarvelPage = () => {
 	return (
 		<div>
 			<h1>MARVEL PAGE</h1>
+			<HeroesList publisher="Marvel"></HeroesList>
 		</div>
 	);
 };
