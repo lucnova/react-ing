b7c4d015c5d349d09e7954131fd543c3ac10d84b
lucnova <nlnovae@gmail.com>
2021-06-04 13:53:24 -0500

    Get Heroes By Publisher

---
+++ REACT/03-React/04-heroes-app/src/components/selectors/getHeroesByPublisher.js
@@ -0,0 +1,11 @@
+import { heroes } from '../../data/heroes';
+
+export const getHeroesByPublisher = (publisher) => {
+	const validPublishers = ['DC Comics', 'Marvel'];
+
+	if (!validPublishers.includes(publisher)) {
+		throw Error('getHeroesByPublisher', publisher, 'is not a valid publisher.', 'Valid publishers:', validPublishers.toString());
+	}
+
+	return heroes.filter((heroe) => heroe.publisher === publisher);
+};
