c2920b8cdb63ec889c3f7663d1147f13bb067c05
lucnova <nlnovae@gmail.com>
2021-06-04 13:54:39 -0500

    HeroesList Added

--- REACT/03-React/04-heroes-app/src/components/DC/DCPage.js
+++ REACT/03-React/04-heroes-app/src/components/DC/DCPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { HeroesList } from '../heroes/HeroesList';
 
 export const DCPage = () => {
 	return (
 		<div>
 			<h1>DC PAGE</h1>
+			<HeroesList publisher="DC Comics"></HeroesList>
 		</div>
 	);
 };
