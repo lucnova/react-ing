23d8aa46e1b129e61adb234f3991a3f33b7fc86b
lucnova <nlnovae@gmail.com>
2021-06-04 13:58:53 -0500

    Container Added

--- REACT/03-React/04-heroes-app/src/routers/DashboardRoutes.js
+++ REACT/03-React/04-heroes-app/src/routers/DashboardRoutes.js
@@ -11,7 +11,7 @@ export const DashboardRoutes = () => {
 		<>
 			<NavBar />
 
-			<div>
+			<div className="container mt-2">
 				<Switch>
 					<Route exact path="/dc" component={DCPage} />
 					<Route exact path="/marvel" component={MarvelPage} />
