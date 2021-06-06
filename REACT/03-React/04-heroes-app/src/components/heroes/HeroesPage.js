5f5dcc245a84bc9e18e0b10e7256e13393755905
lucnova <nlnovae@gmail.com>
2021-06-06 16:13:08 -0500

    Hero not Found redirect

--- REACT/03-React/04-heroes-app/src/components/heroes/HeroesPage.js
+++ REACT/03-React/04-heroes-app/src/components/heroes/HeroesPage.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { Redirect, useParams } from 'react-router';
 import { getHeroById } from '../selectors/getHeroById';
 
 export const HeroesPage = () => {
 	const { heroID } = useParams();
-	const { id, superhero, first_appearance, alter_ego, characters } = getHeroById(heroID);
+
+	// * En caso de que el ID sea uno que no exista entonces redirigo a /
+	const searchedHero = getHeroById(heroID);
+	if (!searchedHero) {
+		return <Redirect to="/" />;
+	}
+
+	const { id, superhero, first_appearance, alter_ego, characters } = searchedHero;
 
 	return (
 		<>
 			<div className="container col-xxl-8 px-4 py-5">
 				<div className="row flex-lg-row-reverse align-items-center g-5 py-5">
 					<div className="col-10 col-sm-8 col-lg-6">
-						<img src={`../assets/heroes/${id}.jpg`} alt={`${id}`} style={{ maxHeight: 512 }} className="rounded shadow" loading="lazy" />
+						<img
+							src={`../assets/heroes/${id}.jpg`}
+							alt={`${id}`}
+							style={{ maxHeight: 512 }}
+							className="rounded shadow"
+							loading="lazy"
+						/>
 					</div>
 					<div className="col-lg-6">
 						<h1 className="display-5 fw-bold lh-1 mb-3">{superhero}</h1>
@@ -23,7 +36,7 @@ export const HeroesPage = () => {
 							{first_appearance}
 							<br />
 							<span className="fw-bold">Characters: </span>
-                            {characters}
+							{characters}
 						</p>
 					</div>
 				</div>
