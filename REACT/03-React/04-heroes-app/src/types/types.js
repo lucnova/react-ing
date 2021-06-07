b6522a72a6e65996a00d943cef512cb42fb9e518
lucnova <nlnovae@gmail.com>
2021-06-07 18:39:16 -0500

    Types for Auth Reducer

---
+++ REACT/03-React/04-heroes-app/src/types/types.js
@@ -0,0 +1,5 @@
+// * OBJETO CON PARES DE VALORES PARA USARSE EN EL REDUCER Y NO PERDER TIEMPO ESCRIBIENDO STRINGS (QUE SON VOLATILES)
+export const types = {
+	login: '[auth] login',
+	logout: '[auth] logout',
+};
