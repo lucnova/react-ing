17144d7e7c6126da38457b9b09beb28cc3ea7c48
lucnova <nlnovae@gmail.com>
2021-06-07 18:24:04 -0500

    NavBar Re-structure

--- REACT/03-React/04-heroes-app/src/components/UI/NavBar.js
+++ REACT/03-React/04-heroes-app/src/components/UI/NavBar.js
@@ -3,33 +3,62 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-			<Link className="navbar-brand" to="/">
-				HEROES
-			</Link>
-
-			<div className="navbar-collapse">
-				<div className="navbar-nav">
-					<NavLink activeClassName="active" className="nav-item nav-link" exact to="/marvel">
-						Marvel
-					</NavLink>
-
-					<NavLink activeClassName="active" className="nav-item nav-link" exact to="/dc">
-						DC
-					</NavLink>
-
-					<NavLink activeClassName="active" className="nav-item nav-link" exact to="/search">
-						<i className="fas fa-search"></i>
-					</NavLink>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<Link className="navbar-brand" to="/">
+					HEROES
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<NavLink activeClassName="active" className="nav-link" exact to="/marvel">
+								Marvel
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink activeClassName="active" className="nav-link" exact to="/dc">
+								DC
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink activeClassName="active" className="nav-link" exact to="/search">
+								<i className="fas fa-search"></i>
+							</NavLink>
+						</li>
+					</ul>
+					<div className="d-flex">
+						<div className="dropdown">
+							<button
+								className="btn btn-secondary dropdown-toggle align-items-center hidden-arrow mx-5"
+								type="button"
+								id="dropdownMenuButton1"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+							>
+								<span>User</span>{' '}
+								<img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" className="rounded-circle" height={25} alt="" loading="lazy" />
+							</button>
+							<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+								<li>
+									<NavLink activeClassName="active" className="dropdown-item" exact to="/login">
+										Login
+									</NavLink>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
-			</div>
-
-			<div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-				<ul className="navbar-nav ml-auto">
-					<NavLink activeClassName="active" className="nav-item nav-link" exact to="/login">
-						Log-In
-					</NavLink>
-				</ul>
 			</div>
 		</nav>
 	);
