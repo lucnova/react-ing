aadb2110641ba48e6e1ad99e16eaa7294cfcd115
lucnova <nlnovae@gmail.com>
2021-06-07 21:26:52 -0500

    Basic AuthContext

--- REACT/03-React/04-heroes-app/src/HeroesApp.js
+++ REACT/03-React/04-heroes-app/src/HeroesApp.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { AuthContext } from './auth/AuthContext';
+import { authReducer } from './auth/authReducer';
 import { AppRouter } from './routers/AppRouter';
 
+// * Leer localstorage (luego sera backend) y si no entonces indicar que no esta logeado
+const init = () => {
+	return (
+		JSON.parse(localStorage.getItem('user')) || {
+			logged: false,
+		}
+	);
+};
+
 export const HeroesApp = () => {
-	return <AppRouter />;
+	const [user, dispatch] = useReducer(authReducer, {}, init);
+
+    // * value Distribuye el objeto a lo largo de la app (perfecto pues el login es necesario)
+	return (
+		<AuthContext.Provider value={{ user, dispatch }}>
+			<AppRouter />
+		</AuthContext.Provider>
+	);
 };
