2ae9cf683cf2a134d39e5af0d6a9c107b39c08a7
Alan <nlnovae@gmail.com>
2021-06-09 17:05:09 -0500

    PrivateRoute implemented outside login page

--- REACT/03-React/04-heroes-app/src/routers/AppRouter.js
+++ REACT/03-React/04-heroes-app/src/routers/AppRouter.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { AuthContext } from '../auth/AuthContext';
 import { LoginPage } from '../components/login/LoginPage';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
+    // * Obtener el contexto (que viene de HeroesApp.js => <AuthContext.Provider value={{ authUser, authDispatch }}></AuthContext.Provider>)
+	const { authUser } = useContext(AuthContext);   // solo extraigo el usuario.
+
 	return (
 		<Router>
 			<Switch>
 				<Route exact path="/login" component={LoginPage} />
 
-				<Route path="/" component={DashboardRoutes} />
+				<PrivateRoute isAuth={authUser.logged} path="/" component={DashboardRoutes} />
 			</Switch>
 		</Router>
 	);
