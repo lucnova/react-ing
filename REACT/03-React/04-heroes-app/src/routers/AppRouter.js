28d4f458098f262a99056273196317aa0e4ca350
Alan <nlnovae@gmail.com>
2021-06-09 17:19:39 -0500

    PublicRoute Implemented

--- REACT/03-React/04-heroes-app/src/routers/AppRouter.js
+++ REACT/03-React/04-heroes-app/src/routers/AppRouter.js
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import { AuthContext } from '../auth/AuthContext';
 import { LoginPage } from '../components/login/LoginPage';
 import { DashboardRoutes } from './DashboardRoutes';
+
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
-    // * Obtener el contexto (que viene de HeroesApp.js => <AuthContext.Provider value={{ authUser, authDispatch }}></AuthContext.Provider>)
-	const { authUser } = useContext(AuthContext);   // solo extraigo el usuario.
+	// * Obtener el contexto (que viene de HeroesApp.js => <AuthContext.Provider value={{ authUser, authDispatch }}></AuthContext.Provider>)
+	const { authUser } = useContext(AuthContext); // solo extraigo el usuario.
 
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="/login" component={LoginPage} />
+				<PublicRoute isAuth={authUser.logged} exact path="/login" component={LoginPage} />
 
 				<PrivateRoute isAuth={authUser.logged} path="/" component={DashboardRoutes} />
 			</Switch>
