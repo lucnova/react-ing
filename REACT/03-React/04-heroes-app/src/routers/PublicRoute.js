03ea09683a9c16f1b508aedabc8fdb92a39b9222
Alan <nlnovae@gmail.com>
2021-06-09 17:19:34 -0500

    PublicRoute Component

---
+++ REACT/03-React/04-heroes-app/src/routers/PublicRoute.js
@@ -0,0 +1,25 @@
+// * COMPONENTE PublicRoute
+// * Es una especie de puente entre ruta y componente, solamente verifica el booleano 'isAuth', y dependiendo de eso
+//      se manda al Route uno u otro componente (pasando los demas props que se incluyan)
+
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+import PropTypes from 'prop-types';
+
+// * ...rest es para pasar todos los props (resto) y hacer un puente entre ruta privada y componente a mostrar.
+export const PublicRoute = ({ isAuth, component: Component, ...rest }) => {
+	return (
+		<Route
+			{...rest}
+			component={(props) => {
+				return !isAuth ? <Component {...props} /> : <Redirect to="/" />;
+			}}
+		/>
+	);
+};
+
+// * PropTypes para el componente
+PublicRoute.propTypes = {
+	isAuth: PropTypes.bool.isRequired,
+	component: PropTypes.func.isRequired,
+};
