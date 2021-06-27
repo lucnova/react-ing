e55b6668161f95d63bdbabc5005d8b4709e43f91
Alan <nlnovae@gmail.com>
2021-06-26 20:01:00 -0500

    Basic Routing Made

--- REACT/03-React/05-journal-app/src/JournalApp.js
+++ REACT/03-React/05-journal-app/src/JournalApp.js
@@ -1,11 +1,7 @@
 import React from 'react';
-
 import './styles/styles.scss';
+import { AppRouter } from './components/routers/AppRouter';
 
 export const JournalApp = () => {
-	return (
-		<div>
-			<h1>JournalApp</h1>
-		</div>
-	);
+	return <AppRouter />;
 };
---
+++ REACT/03-React/05-journal-app/src/components/routers/AuthRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LoginPage } from '../auth/LoginPage';
+import { RegisterPage } from '../auth/RegisterPage';
+
+export const AuthRouter = () => {
+	return (
+		<Switch>
+			<Route path="/auth/login" exact component={LoginPage} />
+			<Route path="/auth/register" exact component={RegisterPage} />
+			<Redirect to="/auth/login"></Redirect>
+		</Switch>
+	);
+};
