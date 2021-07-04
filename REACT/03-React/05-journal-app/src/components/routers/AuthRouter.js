f0162d6b8258abb2adcda61c491bf6bfd3d353e9
Alan <nlnovae@gmail.com>
2021-07-04 15:13:15 -0500

    Container to Router

--- REACT/03-React/05-journal-app/src/components/routers/AuthRouter.js
+++ REACT/03-React/05-journal-app/src/components/routers/AuthRouter.js
@@ -6,11 +6,13 @@ import { RegisterPage } from '../auth/RegisterPage';
 export const AuthRouter = () => {
 	return (
 		<div className="auth__main">
-			<Switch>
-				<Route path="/auth/login" exact component={LoginPage} />
-				<Route path="/auth/register" exact component={RegisterPage} />
-				<Redirect to="/auth/login"></Redirect>
-			</Switch>
+			<div className="auth__box-container">
+				<Switch>
+					<Route path="/auth/login" exact component={LoginPage} />
+					<Route path="/auth/register" exact component={RegisterPage} />
+					<Redirect to="/auth/login"></Redirect>
+				</Switch>
+			</div>
 		</div>
 	);
 };
