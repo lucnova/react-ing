85f27d0cf7c68f7476aaa65f59b87686318c6c39
Alan <nlnovae@gmail.com>
2021-07-04 15:21:53 -0500

    Alt Login (google) + register

--- REACT/03-React/05-journal-app/src/components/auth/LoginPage.js
+++ REACT/03-React/05-journal-app/src/components/auth/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginPage = () => {
 	return (
@@ -12,6 +13,24 @@ export const LoginPage = () => {
 				<button type="submit">Login</button>
 
 				<hr />
+
+				<div>
+					<p>o</p>
+					<div className="google-btn">
+						<div className="google-icon-wrapper">
+							<img
+								className="google-icon"
+								src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+								alt="google button"
+							/>
+						</div>
+						<p className="btn-text">
+							<b>Inicia Sesión con Google</b>
+						</p>
+					</div>
+				</div>
+
+				<Link to="auth/register">Registrarse</Link>
 			</form>
 		</>
 	);
