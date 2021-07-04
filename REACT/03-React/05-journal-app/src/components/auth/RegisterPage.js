73c4ebf006b29f734622762b0b15879570c65663
Alan <nlnovae@gmail.com>
2021-07-04 17:26:47 -0500

    Register Page Added

--- REACT/03-React/05-journal-app/src/components/auth/RegisterPage.js
+++ REACT/03-React/05-journal-app/src/components/auth/RegisterPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const RegisterPage = () => {
 	return (
-		<div>
-			<h1>RegisterPage</h1>
-		</div>
+		<>
+			<h1 className="auth__title">Registrarte</h1>
+
+			<form>
+				<input type="text" placeholder="Nombre" name="name" className="auth__input" autoComplete="off" />
+				<input type="email" placeholder="Correo" name="email" className="auth__input" autoComplete="off" />
+				<input type="password" placeholder="Contraseña" name="password" className="auth__input" autoComplete="off" />
+				<input type="password" placeholder="Confirmar Contraseña" name="password_confirm" className="auth__input" autoComplete="off" />
+
+				<button className="btn btn-primary btn-block" type="submit" disabled={true}>
+					Registrarse
+				</button>
+
+				<hr className="auth__separator" />
+
+				<Link to="/auth/login" className="link">
+					Iniciar Sesión
+				</Link>
+			</form>
+		</>
 	);
 };
