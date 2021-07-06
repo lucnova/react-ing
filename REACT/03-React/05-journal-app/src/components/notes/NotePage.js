9c14b9a5985ed5cb0c4389c1dba1c36abfa9b55f
Alan <nlnovae@gmail.com>
2021-07-06 18:51:43 -0500

    Notes Form Added

--- REACT/03-React/05-journal-app/src/components/notes/NotePage.js
+++ REACT/03-React/05-journal-app/src/components/notes/NotePage.js
@@ -5,6 +5,14 @@ export const NotePage = () => {
 	return (
 		<div className="notepage__main-content">
 			<NotesAppBar />
+
+			<div className="notepage__content">
+				<input type="text" placeholder="Titulo" className="notepage__title" autoComplete="off" name="note_title" />
+				<textarea placeholder="Descripcion" className="notepage__textarea" autoComplete="off" name="note_desc" />
+				<div className="notepage__image">
+					<img src="https://p0.piqsels.com/preview/94/264/999/common-raven-raven-raven-bird-crow.jpg" alt="Imagen" />
+				</div>
+			</div>
 		</div>
 	);
 };
