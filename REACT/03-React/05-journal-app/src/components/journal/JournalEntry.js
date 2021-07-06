8b47368049277b6eb49af1ec2a648e1f5ca9e2c5
Alan <nlnovae@gmail.com>
2021-07-06 18:12:00 -0500

    Clickable Entries

--- REACT/03-React/05-journal-app/src/components/journal/JournalEntry.js
+++ REACT/03-React/05-journal-app/src/components/journal/JournalEntry.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const JournalEntry = () => {
 	return (
-		<div className="journal__entry">
+		<div className="journal__entry clickable">
 			<div
 				className="journal__entry-picture"
 				style={{
