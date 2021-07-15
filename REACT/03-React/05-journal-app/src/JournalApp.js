e7affc499cb7e8e811d23f2be4a4fbf531cc69da
Alan <nlnovae@gmail.com>
2021-07-15 18:00:15 -0500

    Added the Redux Store

--- REACT/03-React/05-journal-app/src/JournalApp.js
+++ REACT/03-React/05-journal-app/src/JournalApp.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './styles/styles.scss';
 import { AppRouter } from './components/routers/AppRouter';
 
+// * En este punto alto de la app puede importarse el store.
+// * Higher Order Component -> Proveedor del Store.
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+
 export const JournalApp = () => {
-	return <AppRouter />;
+	return (
+		<Provider store={store}>
+			<AppRouter />
+		</Provider>
+	);
 };
