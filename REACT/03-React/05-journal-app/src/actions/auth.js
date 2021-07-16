ee9b9d1f7d6aeb2ee3f28ced9838eebf625a618d
Alan <nlnovae@gmail.com>
2021-07-15 19:07:49 -0500

    Async Login Test

--- REACT/03-React/05-journal-app/src/actions/auth.js
+++ REACT/03-React/05-journal-app/src/actions/auth.js
@@ -1,7 +1,15 @@
 import { types } from '../types/types';
 
+export const loginEmailAndPassword = (email, password) => {
+	return (dispatch) => {
+		setTimeout(() => {
+			dispatch(login(email, password));
+		}, 3000);
+	};
+};
+
 export const login = (uid, username) => {
-    // * Es lo mismo que se hacia antes, pero concentrado en un objeto
+	// * Es lo mismo que se hacia antes, pero concentrado en un objeto
 	return {
 		type: types.login,
 		payload: {
